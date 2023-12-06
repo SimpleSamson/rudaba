51a4dc18381040e2dde6249a6a443810ff102b6f
Samson <147044364+SimpleSamson@users.noreply.github.com>
2023-12-06 13:59:46 +0300

    Add files via upload

---
+++ target/debug/deps/rudaba-630219f95d0217ca.d
@@ -0,0 +1,5 @@
+/media/tim/TKQX/projects/rust/rudaba/target/debug/deps/rudaba-630219f95d0217ca: src/main.rs
+
+/media/tim/TKQX/projects/rust/rudaba/target/debug/deps/rudaba-630219f95d0217ca.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/rudaba.d
@@ -0,0 +1,1 @@
+/media/tim/TKQX/projects/rust/rudaba/target/debug/rudaba: /media/tim/TKQX/projects/rust/rudaba/src/main.rs
