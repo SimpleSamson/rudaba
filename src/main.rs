97dc1eec17643cad8bfd65a6e26ccd3f1cb3c9c9
Samson <147044364+SimpleSamson@users.noreply.github.com>
2023-12-07 13:56:38 +0300

    Add files via upload

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,17 @@
 fn main() {
-    let mut username = "guest";
     println!("Welcome {username}");
 }
+
+/*
+    struct for storing database values
+        - databaseName: the name of the database to be created and accessed
+        - username: the name of the user
+ */struct Database{
+    databaseName: String,
+    hostName: String,
+    userName: String,
+
+}
+
+//create a new databse
+fn createDatabase(database: Database, user)
