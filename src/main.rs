0983bfc256195e77a9dcd05b83ce4c3291b04d35
Samson <147044364+SimpleSamson@users.noreply.github.com>
2023-12-14 13:57:15 +0300

    Add files via upload

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::fs::{DirBuilder, File};
+
 fn main() {
     println!("Welcome {username}");
 }
@@ -18,7 +20,22 @@ struct User{
     user_name: String,
     password: String   //should be able to hold encrypted data
 }
+
+//tables
+struct Table{
+    table_name: String,
+}
 //create a new databse
-fn createDatabase(database: Database, user: User){
-    
+fn create_database(database: Database, user: User){
+    //save user details to a config file
+    let databasePath = database.database_name;
+    DirBuilder::new()
+        .create(databasePath).unwrap(); //we do not use .recursive to avoid overwriting the database if it exists
+
+}
+fn create_table(table: Table){
+    //ALL DATABASE FILES ARE IN json format
+    let mut new_table_name = table.table_name + ".json";
+    let mut file = File::create(new_table_name);
+
 }
